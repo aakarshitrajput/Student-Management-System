f382461a05ca33e98edae22b4cf1d58a12a1c7d9
Aakarshit Rajput <aakarshit.rajput23@gmail.com>
2023-02-26 15:23:33 +0530

    base url changed

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,
+    origin: "https://student-management-system8582.netlify.app",
   })
 );
 
