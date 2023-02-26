04c8994b6930166cdce0d44fb5c2769e04281ba2
Aakarshit Rajput <aakarshit.rajput23@gmail.com>
2023-02-26 22:07:29 +0530

    error fixes

--- frontend/src/components/Header/SearchStudent.jsx
+++ frontend/src/components/Header/SearchStudent.jsx
@@ -23,7 +23,7 @@ const SearchStudent = (data) => {
     <div style={{}}>
       <div className="studentContainer">
         <div className="photoContainer">
-          {data.photo === undefined ? (
+          {data.photo.data === undefined ? (
             <img
               src="https://t4.ftcdn.net/jpg/03/59/58/91/360_F_359589186_JDLl8dIWoBNf1iqEkHxhUeeOulx0wOC5.jpg"
               alt="Profile Photo"
