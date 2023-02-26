09fa476ec41263ef76ec966b26c2c4b2f1da8699
Aakarshit Rajput <aakarshit.rajput23@gmail.com>
2023-02-26 16:10:58 +0530

    origin url changed

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "https://student-management-system-backend.onrender.com",
+    origin: process.env.ORIGINURL,
   })
 );
 
